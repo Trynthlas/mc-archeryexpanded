81a89fea0dc72c8b4c5422f82933e87c6aa756a5
Trynthlas <feylan@eternalguard.net>
2015-01-15 23:25:26 -0600

    First commit. Testing stuffs.

---
+++ ArcheryExpanded/src/net/eternalguard/archeryexpanded/ArcheryExpanded.java
@@ -0,0 +1,20 @@
+package net.eternalguard.archeryexpanded;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ArcheryExpanded.MODID, version = ArcheryExpanded.VERSION)
+public class ArcheryExpanded
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
